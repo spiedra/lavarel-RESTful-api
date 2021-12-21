0e317fbdc1fd2a8d9ae6698f324e298457ea19c2
spiedra <jcspiedra@gmail.com>
2021-12-20 23:47:26 -0600

    feat: add GenreSeeder file for database test

--- database/seeders/GenreSeeder.php
+++ database/seeders/GenreSeeder.php
@@ -2,17 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Genre;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class GenreSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-       $this->call(GenreSeeder::class); 
+        //
     }
 }
