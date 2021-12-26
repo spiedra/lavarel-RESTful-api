1f06bfdd09702e66caee325eada35b1f4ac3d6d1
spiedra <jcspiedra@gmail.com>
2021-12-25 18:07:01 -0600

    content: update urls address

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,27 +21,27 @@
             <div class="container__route-content">
                 <h3>Index</h3>
                 <p>Description: Displays a list of genres</p>
-                <code>GET: /api/genres</code>
+                <code>GET: /public/api/genres</code>
             </div>
             <div class="container__route-content">
                 <h3>Store</h3>
                 <p>Description: Stores a new genre in the database</p>
-                <code>POST: /api/genres</code>
+                <code>POST: /public/api/genres</code>
             </div>
             <div class="container__route-content">
                 <h3>Show</h3>
                 <p>Description: Displays the specified genre</p>
-                <code>GET: /api/genres/id</code>
+                <code>GET: /public/api/genres/id</code>
             </div>
             <div class="container__route-content">
                 <h3>Update</h3>
                 <p>Description: Displays the specified genre</p>
-                <code>PUT: /api/genres/{genre}/id</code>
+                <code>PUT: /public/api/genres/{genre}/id</code>
             </div>
             <div class="container__route-content">
                 <h3>Delete</h3>
                 <p>Description: Removes the specified genre from database</p>
-                <code>DELETE: /api/genres/id</code>
+                <code>DELETE: /public/api/genres/id</code>
             </div>
         </div>
         <hr />
@@ -50,27 +50,27 @@
             <div class="container__route-content">
                 <h3>Index</h3>
                 <p>Description: Displays a list of movie</p>
-                <code>GET: /api/movies</code>
+                <code>GET: /public/api/movies</code>
             </div>
             <div class="container__route-content">
                 <h3>Store</h3>
                 <p>Description: Stores a new movie in the database</p>
-                <code>POST: /api/movies</code>
+                <code>POST: /public/api/movies</code>
             </div>
             <div class="container__route-content">
                 <h3>Show</h3>
                 <p>Description: Displays the specified movie</p>
-                <code>GET: /api/movies/id</code>
+                <code>GET: /public/api/movies/id</code>
             </div>
             <div class="container__route-content">
                 <h3>Update</h3>
                 <p>Description: Displays the specified movie</p>
-                <code>PUT: /api/movies/{movie}/id</code>
+                <code>PUT: /public/api/movies/{movie}/id</code>
             </div>
             <div class="container__route-content">
                 <h3>Delete</h3>
                 <p>Description: Removes the specified movie from database</p>
-                <code>DELETE: /api/movies/id</code>
+                <code>DELETE: /public/api/movies/id</code>
             </div>
         </div>
     </main>
